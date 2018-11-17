77faed4671e3ed2411460ce01db1a46388484452
t-oki <blutac.nz@gmail.com>
2018-11-17 19:17:23 +0900

    2018/11/17

---
+++ volume0/0009_Prime_Number/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var max = 100000
+var sc = bufio.NewScanner(os.Stdin)
+
+func main() {
+	for sc.Scan() {
+		a, _ := strconv.Atoi(sc.Text())
+		fmt.Println(countPrime(a))
+	}
+}
+
+func countPrime(a int) int {
+	primes := make([]bool, a+1)
+	n := 0
+	for i := 2; i <= a; i++ {
+		if primes[i] == false {
+			n++
+			for j := i * i; j <= a; j += i {
+				primes[j] = true
+			}
+		}
+	}
+	return n
+}
