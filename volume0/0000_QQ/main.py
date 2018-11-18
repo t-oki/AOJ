8a191479c2b202bd7eaa8fe76e209d2abb7c67f9
t-oki <blutac.nz@gmail.com>
2018-11-18 20:07:21 +0900

    2018/11/18

---
+++ volume0/0000_QQ/main.py
@@ -0,0 +1,7 @@
+def printQQ():
+  for i in range(1,10):
+    for j in range(1,10):
+      print("{}x{}={}".format(i, j, i*j))
+
+if __name__ == "__main__":
+  printQQ()
---
+++ volume0/0001_List_of_Top3_Hills/main.py
@@ -0,0 +1,7 @@
+heights = []
+for _ in range(10):
+  heights.append(int(input()))
+heights.sort()
+print(heights[-1])
+print(heights[-2])
+print(heights[-3])
